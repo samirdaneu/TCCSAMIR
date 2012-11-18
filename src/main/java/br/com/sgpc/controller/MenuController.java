0fbbafa80823d631f6083976f1a80c086178caf3
samirdaneu <samir.daneu@gmail.com>
2012-11-18 16:26:39 -0200

    #17

--- src/main/java/br/com/sgpc/controller/MenuController.java
+++ src/main/java/br/com/sgpc/controller/MenuController.java
@@ -13,13 +13,61 @@ import br.com.sgpc.model.Usuario;
  * @since 01/10/2012
  *
  */
-@ManagedBean(name = "menu")
+@ManagedBean(name = "menuController")
 @RequestScoped
 public class MenuController implements Serializable {
 
 	private static final long serialVersionUID = 8939415106345805141L;
 	
 	private Usuario usuario;
+	
+	public String consultarUsuarios(){
+		return "/usuario/mostrarUsuarios";
+	}
+	
+	public String consultarProdutos(){
+		return "/produto/mostrarProdutos";
+	}
+	
+	public String consultarFornecedores(){
+		return "/fornecedor/mostrarFornecedores";
+	}
+	
+	public String consultarMovimentacoes(){
+		return "/movimentacao/mostrarMovimentacoes";
+	}
+	
+	public String consultarPedidos(){
+		return "/pedido/mostrarPedidos";
+	}
+	
+	public String consultarVendas(){
+		return "/relatorio/formRelatorioVendas";
+	}
+	
+	public String cadastrarProduto(){
+		return "/produto/formProduto";
+	}
+	
+	public String cadastrarFornecedor(){
+		return "/fornecedor/formFornecedor";
+	}
+	
+	public String cadastrarUsuario(){
+		return "/usuario/formUsuario";
+	}
+	
+	public String cadastrarMovimentacao(){
+		return "/movimentacao/formMovimentacao";
+	}
+	
+	public String alterarSenha(){
+		return "/usuario/formAlterarSenha";
+	}
+	
+	public String cadastrarPedido(){
+		return "/pedido/formPedido";
+	}
 
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
